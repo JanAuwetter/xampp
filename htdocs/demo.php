f602771b2eb148b9db00cc36ac8a1459fdd006c0
JanAuwetter <goliathxx@gmx.de>
2021-03-12 09:24:44 +0100

    PHP Start

---
+++ htdocs/demo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Demo</h1>
+    <?php
+        echo "Hallo Welt!";
+        echo "<h2>Hallo aus PHP</h2>";
+        $Wert
+      ?>
+  </body>
+</html>
